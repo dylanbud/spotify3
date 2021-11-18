4942a8533a612bd9ad2399db4c2b4a073bd6208b
Dylan Budnick <dylanjbudnick@gmail.com>
2021-11-18 12:35:28 -0800

    Updated recommendations.py and templates to deal with errors, application will now tell you when a song cannot be found.

--- spotify/app.py
+++ spotify/app.py
@@ -1,6 +1,6 @@
-from flask import Flask,render_template,request
-from .user_input import *
+from flask import Flask, render_template, request
 from .recommendation import *
+
 
 def create_app():
     # initializes our app
@@ -10,19 +10,16 @@ def create_app():
     def form():
         return render_template('base.html')
 
-    @app.route('/data/', methods = ['POST', 'GET'])
-
+    @app.route('/data/', methods=['POST', 'GET'])
     def data():
         if request.method == 'GET':
-            return f"The URL /data is accessed directly. Try going to '/form' to submit form"
+            return f"/data is accessed directly. Go to '/' to submit form"
         if request.method == 'POST':
             form_data = request.form
             artist = request.form.get('artist', 'default')
             track = request.form.get('track', 'default')
             df = convert(artist, track)
             rec = get_recommendation(track, artist)
-        return render_template('data.html',form_data = form_data, artist=artist, track=track, df = df, rec=rec)
-  
+        return render_template('data.html', form_data=form_data, artist=artist, track=track, df=df, rec=rec)
+
     return app
-
-
--- spotify/recommendation.py
+++ spotify/recommendation.py
@@ -17,17 +17,25 @@ Song_data = pd.read_csv('spotify/Song_data.csv', index_col=0)
 def convert(artist, track):
     track_search = spotify.search(
         q='artist:' + artist + ' track:' + track, type='track', limit=1, market='US')
-    track_id = track_search['tracks']['items'][0]['id']
-    user_audio = spotify.audio_features(track_id)
-    df = pd.DataFrame.from_records(user_audio)
-    df.drop(columns=['key', 'type', 'id', 'uri', 'track_href',
-            'analysis_url', 'duration_ms', 'time_signature'], inplace=True)
-    df.to_numpy().reshape(1, -1)
+    if track_search['tracks']['total'] == 0:
+        df = pd.DataFrame(data={'danceability': [3]})
+        return df
+    else:
+        track_id = track_search['tracks']['items'][0]['id']
+        user_audio = spotify.audio_features(track_id)
+        df = pd.DataFrame.from_records(user_audio)
+        df.drop(columns=['key', 'type', 'id', 'uri', 'track_href',
+                'analysis_url', 'duration_ms', 'time_signature'], inplace=True)
+        df.to_numpy().reshape(1, -1)
     return (df)
 
 
 def get_recommendation(artist, track):
     user_song_data = convert(artist, track)
-    rec_raw = model.kneighbors(user_song_data, 5, return_distance=False)
-    rec_list = rec_raw.tolist()[0]
-    return Song_data.iloc[rec_list]
+    if user_song_data.iloc[0][0] > 1:
+        Song_data_error = pd.DataFrame(data={'Error finding song': ['Please use a different song']})
+        return Song_data_error
+    else:
+        rec_raw = model.kneighbors(user_song_data, 5, return_distance=False)
+        rec_list = rec_raw.tolist()[0]
+        return Song_data.iloc[rec_list]
