c285908753a62c21a405e6a57af5bfb8bb2c0e65
ZacharyRock <86319455+ZacharyRock@users.noreply.github.com>
2021-11-17 18:58:40 -0600

    Update recommendation.py

--- spotify/recommendation.py
+++ spotify/recommendation.py
@@ -3,21 +3,31 @@ import numpy as np
 from sklearn.neighbors import KNeighborsClassifier
 import pickle
 import requests
+from os import getenv
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 
-
-def api(user_song_data_fetch):
-    dummy_data = np.array([[ 3.88000e-01,  8.79000e-01, -6.47300e+00,  0.00000e+00,
-         3.72000e-02,  2.07000e-03,  9.28000e-05,  2.95000e-01,
-         5.80000e-01,  1.56434e+02]])
-    return dummy_data
-
+# Initalize spotipy
+spotify = spotipy.Spotify(
+    client_credentials_manager=SpotifyClientCredentials())
 model = pickle.load(open('model.pkl', 'rb'))
 Song_data = pd.read_csv('Song_data.csv', index_col=0)
 
-def get_recommendation(user_song):
-    user_song_data = api(user_song)
-    rec_raw = model.kneighbors(user_song_data, 5, return_distance=False)
-    rec_list=rec_raw.tolist()[0]
-    return Song_data.iloc[rec_list]
 
-# get_recommendation('The User Input')
+def convert(artist, track):
+    track_search = spotify.search(
+        q='artist:' + artist + ' track:' + track, type='track', limit=1, market='US')
+    track_id = track_search['tracks']['items'][0]['id']
+    user_audio = spotify.audio_features(track_id)
+    df = pd.DataFrame.from_records(user_audio)
+    df.drop(columns=['key', 'type', 'id', 'uri', 'track_href',
+            'analysis_url', 'duration_ms', 'time_signature'], inplace=True)
+    df.to_numpy().reshape(1, -1)
+    return (df)
+
+
+def get_recommendation(user_song, user_artist_name):
+    user_song_data = convert(user_song, user_artist_name)
+    rec_raw = model.kneighbors(user_song_data, 5, return_distance=False)
+    rec_list = rec_raw.tolist()[0]
+    return Song_data.iloc[rec_list]
