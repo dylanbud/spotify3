3c2ee66122604e361ad53514cb618dcfaab24e9f
Dylan Budnick <dylanjbudnick@gmail.com>
2021-11-17 16:47:45 -0800

    Added basic html views, modified user_input.py to add a def to collect and transform user data into a df readable by our model.

--- spotify/app.py
+++ spotify/app.py
@@ -1,11 +1,25 @@
-from flask import Flask
+from flask import Flask,render_template,request
+from .user_input import *
 def create_app():
     # initializes our app
     app = Flask(__name__)
 
-    @app.route("/")
-    def hello_world():
-        return "<p>Hello, World!</p>"
+    @app.route('/')
+    def form():
+        return render_template('base.html')
 
-        
+    @app.route('/data/', methods = ['POST', 'GET'])
+
+    def data():
+        if request.method == 'GET':
+            return f"The URL /data is accessed directly. Try going to '/form' to submit form"
+        if request.method == 'POST':
+            form_data = request.form
+            artist = request.form.get('artist', 'default')
+            track = request.form.get('track', 'default')
+            df = convert(artist, track)
+        return render_template('data.html',form_data = form_data, artist=artist, track=track, df = df)
+  
     return app
+
+
--- spotify/user_input.py
+++ spotify/user_input.py
@@ -1,20 +1,19 @@
 '''Query Spotify API based on Artist name and Track, convert using 'audio analysis' to match comparasion dataset'''
 
+from os import getenv
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
+import pandas as pd
+import numpy as np
 
 # Initalize spotipy
 spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
 
-# Get user input, Artist & Track
-artist = input('Enter an artist: ')
-track = input('Enter a track name: ')
-
-# Query Spotify for artist & track name, return first item
-track_search= spotify.search(q='artist:' + artist + ' track:' + track, type='track', limit=1, market='US')
-track_id = track_search['tracks']['items'][0]['id']
-
-# Convert result to format for comparison with dataset
-user_audio = spotify.audio_analysis(track_id)
-
-print(user_audio)
+def convert(artist, track):
+    track_search= spotify.search(q='artist:' + artist + ' track:' + track, type='track', limit=1, market='US')
+    track_id = track_search['tracks']['items'][0]['id']
+    user_audio = spotify.audio_features(track_id)
+    df = pd.DataFrame.from_records(user_audio)
+    df.drop(columns = ['key', 'type', 'id', 'uri', 'track_href', 'analysis_url', 'duration_ms', 'time_signature'], inplace=True)
+    df.to_numpy().reshape(1,-1)
+    return (df)
